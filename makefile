bc1595abd453194c1594a8e20b7a31959a15c2ae
Owen Trueblood <hi@owentrueblood.com>
2019-08-24 20:18:08 -0400

    Send frames to array

--- makefile
+++ makefile
@@ -2,6 +2,6 @@ SOURCES=src/*.java
 OLD_JDK_BIN=/Users/owen/Downloads/jdk-9.0.4.jdk/Contents/Home/bin
 
 library/sws.jar: $(SOURCES)
-	$(OLD_JDK_BIN)/javac -source 1.6 -target 1.6 -classpath /Applications/Processing.app/Contents/Java/core/library/core.jar -d build $(SOURCES)
+	$(OLD_JDK_BIN)/javac -Xlint:-options -source 1.6 -target 1.6 -classpath /Applications/Processing.app/Contents/Java/core/library/core.jar -d build $(SOURCES)
 	$(OLD_JDK_BIN)/jar -cf sws_control.jar -C build .
 	mv sws_control.jar library
--- src/SpaceWithinSpacesControl.java
+++ src/SpaceWithinSpacesControl.java
@@ -1,15 +1,70 @@
 package emotivemachine.sws_control;
+import java.io.IOException;
+import java.net.*;
+
 import processing.core.*;
 
 public class SpaceWithinSpacesControl {
-  PApplet parent;
+  private final static int ARRAY_WIDTH = 10;
+  private final static int ARRAY_HEIGHT = 18;
+  private final static int ARRAY_PORT = 4210;
+  private InetAddress BROADCAST_ADDRESS;
+
+  private PApplet parent;
+
+  private DatagramSocket sock;
 
   public SpaceWithinSpacesControl(PApplet parent) {
     this.parent = parent;
+    parent.registerMethod("dispose", this);
     parent.registerMethod("post", this);
+
+    try {
+      BROADCAST_ADDRESS = InetAddress.getByName("255.255.255.255");
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+
+    try {
+      this.sock = new DatagramSocket();
+      this.sock.setBroadcast(true);
+    } catch (SocketException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private byte[] imageToBytes(PImage image) {
+    byte[] bytes = new byte[ARRAY_WIDTH * ARRAY_HEIGHT];
+
+    for (int y = 0; y < image.height; y++) {
+      for (int x = 0; x < image.width; x++) {
+        int brightness = (int)parent.brightness(image.get(x, y));
+        bytes[y * ARRAY_WIDTH + x] = (byte)brightness;
+      }
+    }
+
+    return bytes;
+  }
+
+  private void updateLEDArray(PImage frame) {
+    byte[] imageBytes = imageToBytes(frame);
+    DatagramPacket packet = new DatagramPacket(imageBytes, imageBytes.length, BROADCAST_ADDRESS, ARRAY_PORT);
+
+    try {
+      this.sock.send(packet);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   public void post() {
-    System.out.println("Hello world");
+    PImage canvasImage = parent.get();
+    canvasImage.resize(ARRAY_WIDTH, ARRAY_HEIGHT);
+    updateLEDArray(canvasImage);
+    System.out.println("SWS: Sent pixels to array");
+  }
+
+  public void dispose() {
+    this.sock.close();
   }
 }
